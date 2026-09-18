961cb521674b34cf8365613f410db0a77e455a68
pratik^C <pratikrajpr1618@gmail.com^C>
2026-09-18 15:04:34 +0530

    Fix frontend backend connection

--- main.py
+++ main.py
@@ -27,10 +27,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        "https://blood-bridge-pink.vercel.app",
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
